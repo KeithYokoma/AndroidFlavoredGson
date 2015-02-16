63b439dc104e17464c8510ae970f6dcd06630da0
KeithYokoma <Keith.Yokoma@gmail.com>
2015-02-16 14:53:19 +0900

    Fix tests

--- lib/src/test/java/jp/yokomark/utils/gson/AospGsonBuilderTest.java
+++ lib/src/test/java/jp/yokomark/utils/gson/AospGsonBuilderTest.java
@@ -6,17 +6,13 @@ import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
 
 import jp.yokomark.utils.gson.mock.SomeEntity;
 
+@Config(emulateSdk = 18)
 @RunWith(RobolectricGradleTestRunner.class)
 public class AospGsonBuilderTest {
-    @Test
-    public void buildInternalUseGson()  throws Exception {
-        Gson gson = AospGsonBuilder.buildInternalUseGson();
-        Assert.assertNotNull(gson);
-    }
-
     @Test
     public void buildExternalUseGson()  throws Exception {
         Gson gson = AospGsonBuilder.build();
@@ -24,23 +20,8 @@ public class AospGsonBuilderTest {
     }
 
     @Test
-    public void convertWithInternalUseGson() throws Exception {
-        Gson gson = AospGsonBuilder.buildInternalUseGson();
-        String converted = gson.toJson(new SomeEntity());
-
-        JSONObject obj = new JSONObject(converted);
-        Assert.assertEquals(1, obj.getInt("id"));
-        Assert.assertEquals("hoge", obj.getString("hoge"));
-        Assert.assertEquals("foo", obj.getString("type"));
-        Assert.assertEquals("Android", obj.getString("name"));
-        Assert.assertFalse(obj.has("should_excluded"));
-        Assert.assertFalse(obj.has("constant"));
-        Assert.assertFalse(obj.has("CONSTANT"));
-    }
-
-    @Test
     public void convertWithExternalUseGson() throws Exception {
-        Gson gson = AospGsonBuilder.buildInternalUseGson();
+        Gson gson = AospGsonBuilder.build();
         String converted = gson.toJson(new SomeEntity());
 
         JSONObject obj = new JSONObject(converted);
